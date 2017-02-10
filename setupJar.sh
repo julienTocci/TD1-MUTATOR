78e89f49418ed22c3c806a7e34214285a74fcb07
julienTocci <julien.tocci@gmail.com>
2017-02-10 16:28:42 +0100

    new script

--- setupJar.sh
+++ setupJar.sh
@@ -1,8 +1,9 @@
-mvn clean
-mvn compile
 mvn package
 cp ./target/devops-1.0-SNAPSHOT.jar ../TD1-DEVOPS/src/main/resources/
 cd ../TD1-DEVOPS/
-mvn clean
-mvn compile
-mvn package
+
+i=1;
+name="Mutant""$i " 
+touch ../error_test.log
+tests_error=$(mvn package | grep "Tests run:")
+echo "$name""$tests_error" >> ../error_test.log
