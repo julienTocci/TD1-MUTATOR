d9842695255808f6ab61b424b1325d5a21ff6e5d
ThomasGillot <gillot.thomas@etu.unice.fr>
2017-02-10 15:46:15 +0100

    Mutator qui change les && en || et inversement

--- src/main/java/CatchProcessor.java
+++ src/main/java/CatchProcessor.java
@@ -4,6 +4,7 @@ import spoon.processing.AbstractProcessor;
 import spoon.reflect.code.BinaryOperatorKind;
 import spoon.reflect.code.CtBinaryOperator;
 import spoon.reflect.code.CtCatch;
+import spoon.reflect.declaration.CtElement;
 
 import java.util.Arrays;
 import java.util.List;
@@ -12,27 +13,30 @@ import java.util.Random;
 /**
  * Reports warnings when empty catch blocks are found.
  */
-public class CatchProcessor extends AbstractProcessor<CtCatch> {
+public class CatchProcessor extends AbstractProcessor<CtElement> {
 
     private static final Random random = new Random();
 
-    public void process(CtCatch element) {
-        if (element.getBody().getStatements().size() == 0) {
-            // for instance we can mutate operators
-            if (element instanceof CtBinaryOperator<?>) {
-                CtBinaryOperator<?> op = (CtBinaryOperator<?>)element;
-                List<BinaryOperatorKind> arithmeticOperators =
-                Arrays.asList(
-                        BinaryOperatorKind.PLUS,
-                        BinaryOperatorKind.MINUS,
-                        BinaryOperatorKind.MUL,
-                        BinaryOperatorKind.DIV
-                );
-                // let's mutate arithmethic operators
-                if (arithmeticOperators.contains(op.getKind())) {
-                    op.setKind(arithmeticOperators.get(random.nextInt(arithmeticOperators.size())));
-                }
+    public void process(CtElement element) {
 
+        // for instance we can mutate operators
+        if (element instanceof CtBinaryOperator<?>) {
+            CtBinaryOperator<?> op = (CtBinaryOperator<?>) element;
+            List<BinaryOperatorKind> arithmeticOperators =
+                    Arrays.asList(
+                            BinaryOperatorKind.PLUS,
+                            BinaryOperatorKind.MINUS,
+                            BinaryOperatorKind.MUL,
+                            BinaryOperatorKind.DIV,
+                            BinaryOperatorKind.AND, // &&
+                            BinaryOperatorKind.OR
+                    );
+            // let's mutate arithmethic operators
+            if (arithmeticOperators.contains(op.getKind())) {
+                if (op.getKind() == BinaryOperatorKind.AND)
+                    op.setKind(BinaryOperatorKind.OR);
+                else if (op.getKind() == BinaryOperatorKind.OR)
+                    op.setKind(BinaryOperatorKind.AND);
             }
         }
     }
