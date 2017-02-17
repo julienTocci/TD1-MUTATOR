2db3972dd7ffef07eaf92e8e5670b1c5eee00d15
julienTocci <julien.tocci@gmail.com>
2017-02-17 15:39:10 +0100

    New script run

--- runLoopMutator.sh
+++ runLoopMutator.sh
@@ -1,5 +1,21 @@
 i=1;
-name="Mutant""$i " 
-touch ../error_test.log
-tests_error=$(mvn package | grep "Tests run:")
-echo "$name""$tests_error" >> ../error_test.log
+
+cd src/main/java
+
+function check {
+	rm ../../../processorName.out
+	for i in *;
+	do
+        echo "$i" | sed 's/.java//g' >> ../../../processorName.out
+		
+    done
+	cat ../../../processorName.out
+
+}
+
+check
+
+#name="Mutant""$i " 
+#touch ../error_test.log
+#tests_error=$(mvn package | grep "Tests run:")
+#echo "$name""$tests_error" >> ../error_test.log
