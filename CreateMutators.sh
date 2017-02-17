314f90ebde6db1fb4d5a22cf2ea49b8cdff2599e
julienTocci <julien.tocci@gmail.com>
2017-02-17 15:13:50 +0100

    New script run

---
+++ CreateMutators.sh
@@ -0,0 +1,3 @@
+mvn package
+cp ./target/devops-1.0-SNAPSHOT.jar ../TD1-DEVOPS/src/main/resources/
+cd ../TD1-DEVOPS/
--- runLoopMutator.sh
+++ runLoopMutator.sh
@@ -1,7 +1,3 @@
-mvn package
-cp ./target/devops-1.0-SNAPSHOT.jar ../TD1-DEVOPS/src/main/resources/
-cd ../TD1-DEVOPS/
-
 i=1;
 name="Mutant""$i " 
 touch ../error_test.log
